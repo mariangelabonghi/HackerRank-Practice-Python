b80d51a793fef61ff32687c8081dc1b7cbf657b8
Mariangela Bonghi <mariangela.bonghi@gmail.com>
2021-01-03 11:38:54 +0100

    feat: Add file print_input.py

---
+++ print_input.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+#read from input stdin and save to input_string
+input_string=input()
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+# Print the contents of input_string to stdout.
+print(input_string)
