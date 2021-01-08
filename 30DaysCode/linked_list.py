2e4783ad44c8e778fbbd62661702d272fbce4da9
Mariangela Bonghi <mariangela.bonghi@gmail.com>
2021-01-08 17:13:10 +0100

    feat: Add file 30DaysCode/linked_list.py

---
+++ 30DaysCode/linked_list.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        new_node = Node(data)
+        if head is None:
+            head = new_node
+        else:
+            current_node = head
+            while current_node.next != None:
+                current_node = current_node.next
+            current_node.next = new_node
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+mylist.display(head); 	  
