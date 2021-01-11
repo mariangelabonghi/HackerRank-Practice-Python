ba038f552df4f1d28a8577b67bb51eff2e3a6a74
Mariangela Bonghi <mariangela.bonghi@gmail.com>
2021-01-11 10:43:38 +0100

    feat: Add file ProblemSolving/compare_triplets.py

---
+++ ProblemSolving/compare_triplets.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def compareTriplets(a, b):
+    score=[0,0]
+    for i in range(len(a)):
+        if a[i]>b[i]:
+            score[0]+=1
+        if  a[i]<b[i]:
+            score[1]+=1
+    return score
+if __name__ == '__main__':
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+    result = compareTriplets(a, b)
+    print(result)
