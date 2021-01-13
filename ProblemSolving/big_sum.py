8e8dad7f6b7357bae1085754d0131992e5e81032
Mariangela Bonghi <mariangela.bonghi@gmail.com>
2021-01-13 17:38:32 +0100

    feat: Add file ProblemSolving/big_sum.py

---
+++ ProblemSolving/big_sum.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = aVeryBigSum(ar)
+    print(result)
