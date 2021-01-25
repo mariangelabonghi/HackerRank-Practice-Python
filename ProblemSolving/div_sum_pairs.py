b5f83117d6055708fd6935a80900ae806db70258
Mariangela Bonghi <mariangela.bonghi@gmail.com>
2021-01-25 19:06:40 +0100

    feat: Add file ProblemSolving/div_sum_pairs.py

---
+++ ProblemSolving/div_sum_pairs.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    count=0
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if (ar[i]+ar[j])%k==0:
+                count+=1
+    return count
+if __name__ == '__main__':
+    nk = input().split()
+    n = int(nk[0])
+    k = int(nk[1])
+    ar = list(map(int, input().rstrip().split()))
+    result = divisibleSumPairs(n, k, ar)
+    print(result)
